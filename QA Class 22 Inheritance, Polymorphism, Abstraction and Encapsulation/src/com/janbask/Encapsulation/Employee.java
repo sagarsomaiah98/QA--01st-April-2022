766e47ecd7178b412a361d50070c9a307ebe69d6
Som <somaiah1988@gmail.com>
2022-05-04 20:50:17 -0400

    04

---
+++ QA Class 22 Inheritance, Polymorphism, Abstraction and Encapsulation/src/com/janbask/Encapsulation/Employee.java
@@ -0,0 +1,26 @@
+package com.janbask.Encapsulation;
+
+public class Employee {
+	
+	private String name;
+	
+	
+	public void setname(String name) {
+		
+		this.name=name;
+	}
+	
+	
+	public String getname() {
+		
+		return name;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ QA Class 22 Inheritance, Polymorphism, Abstraction and Encapsulation/src/com/janbask/Encapsulation/Test.java
@@ -0,0 +1,14 @@
+package com.janbask.Encapsulation;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		Employee e = new Employee();
+		e.setname("Joe");
+		
+		System.out.println(e.getname());
+
+	}
+
+}
